92ab633dcb1fb173f53264912aa3001305169227
Pedro Vega <103451058+peython7@users.noreply.github.com>
2025-09-29 22:01:24 -0300

    Create Accion.java

---
+++ Ejercicio5/Accion.java
@@ -0,0 +1,19 @@
+package ar.edu.unlp.objetos.uno.ejercicio5;
+
+public class Accion {
+
+	private String nombre;
+	private double valorUnitario;
+	
+	public double getValor() {
+		return this.valorUnitario;
+	}
+	
+	public void setNombre(String n) {
+		this.nombre = n;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+}
